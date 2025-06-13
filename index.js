1597a698056492b3dac37e3b46b16148531325a0
slasher-765 <shreyashkumar61@gmail.com>
2025-06-13 16:57:43 +0530

    Fixed: use dynamic port for Render

--- index.js
+++ index.js
@@ -56,5 +56,8 @@ app.get('/files/:filename', (req, res) => {
 
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
